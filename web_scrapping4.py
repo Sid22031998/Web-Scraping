a337bdcee4eda7d40d4c6aa0d15862ff80fe81a7
Siddharth Singh <31348093+Sid22031998@users.noreply.github.com>
2020-09-10 13:50:53 +0530

    Update web_scrapping4.py

--- web_scrapping4.py
+++ web_scrapping4.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.snapdeal.com/search?keyword=keyboard%20mouse&santizedKeyword=&catId=&categoryId=0&suggested=true&vertical=p&noOfResults=20&searchState=&clickSrc=suggested&lastKeyword=&prodCatId=&changeBackToAll=false&foundInAll=false&categoryIdSearched=&cityPageUrl=&categoryUrl=ALL&url=&utmContent=&dealDetail=&sort=rlvncy"
+url = "https://keyboard-details" 
 req=requests.get(url).text
 # print(req)
 soup=BeautifulSoup(req, "html.parser")
 containers=soup.find_all("div", class_="favDp")
 
-filename="snapdeal_keyboard.csv"
+filename="****_keyboard.csv"
 f=open(filename, "w")
 headers="Brand,Product_Name,Price\n"
 f.write(headers)
